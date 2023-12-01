d208aea2b6ba51bf2832968d76ac63430f0cb18b
Laura Ketola <laura.ketola@gofore.com>
2023-12-01 09:45:16 +0200

    assert correct results

--- src/aoc2023/day1.clj
+++ src/aoc2023/day1.clj
@@ -3,8 +3,7 @@
   (:require [clojure.java.io :as io])
   (:require [aoc2023.util :as u]))
 
-(def words {
-            "one" 1
+(def words {"one" 1
             "two" 2
             "three" 3
             "four" 4
@@ -14,15 +13,14 @@
             "eight" 8
             "nine" 9})
 
-
 (defn replace-first-word [line]
   (str/replace-first line #"(?<!\d\S*)(one|two|three|four|five|six|seven|eight|nine)"
-               (fn [[_ a]] (str (get words a)))))
+                     (fn [[_ a]] (str (get words a)))))
 
 (defn replace-last-word [line]
   ;; :DDDD
   (str/reverse (str/replace-first (str/reverse line) #"(eno|owt|eerht|ruof|evif|xis|neves|thgie|enin)"
-                              (fn [[_ a]] (str (get words (str/reverse a)))))))
+                                  (fn [[_ a]] (str (get words (str/reverse a)))))))
 
 (defn replace-words [line]
   (-> line
@@ -48,7 +46,7 @@
        (map u/str->int)
        (apply +)))
 
-(part1 "day1_input")
-(part2 "day1_input")
-(part2 "day1_example")
-(part2 "day1_example2")
+(u/varmista part1 "day1_example" 142)
+(u/varmista part2 "day1_example2" 281)
+(u/varmista part1 "day1_input" 52974)
+(u/varmista part2 "day1_input" 53340)
