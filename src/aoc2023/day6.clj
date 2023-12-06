d307e6d10d5c4932eb06a3746345af2bbe80c2fe
Laura Ketola <laura.ketola@gofore.com>
2023-12-06 14:34:21 +0200

    day 6: algebraic solution

--- src/aoc2023/day6.clj
+++ src/aoc2023/day6.clj
@@ -1,13 +1,11 @@
 (ns aoc2023.day6
-  (:require [clojure.string :as str])
-  (:require [clojure.math :as math])
-  (:require [clojure.java.io :as io])
-  (:require [aoc2023.util :as u]))
+  (:require [clojure.string :as str]
+            [clojure.math :as math]
+            [clojure.java.io :as io]
+            [aoc2023.util :as u]))
 
 (defn travel-factory [x]
-  (let [max-travel (int (/ x 2))
-        travel-by-press-time (fn [y] (- (* x y) (* y y)))]
-    [max-travel travel-by-press-time]))
+  (fn [y] (- (* x y) (* y y))))
 
 (defn parse [lines]
   (let [times (re-seq #"\d+" (first lines))
@@ -20,15 +18,17 @@
         distances (re-seq #"\d+" (second lines))]
     [(u/str->long (apply str times)) (u/str->long (apply str distances))]))
 
-(defn find-limit [f time space dir-f]
-  (if (< space (f (dir-f time)))
-    (recur f (dir-f time) space dir-f)
-    time))
+(defn find-limits [f x l]
+  (let [r1 (int (math/ceil (/ (- x (math/sqrt (- (* x x) (* 4 l)))) 2)))
+        r2 (int (math/floor (/ (+ x (math/sqrt (- (* x x) (* 4 l)))) 2)))
+        v1 (f r1)
+        v2 (f r2)]
+    [(if (= v1 l) (inc r1) r1)
+     (if (= v2 l) (dec r2) r2)]))
 
 (defn find-wins [[time distance]]
-  (let [[best solve] (travel-factory time)
-        smallest (find-limit solve best distance dec)
-        largest (find-limit solve best distance inc)]
+  (let [solve (travel-factory time)
+        [smallest largest] (find-limits solve time distance)]
     (inc (- largest smallest))))
 
 (defn part1 [filename]
