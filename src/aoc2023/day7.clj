e02d7ad10b0e2c0eadf3a9ce004a32d70ac1d490
Laura Ketola <laura.ketola@gofore.com>
2023-12-07 12:04:01 +0200

    Day 7: jokerify jokerify jokerify

--- src/aoc2023/day7.clj
+++ src/aoc2023/day7.clj
@@ -5,39 +5,37 @@
             [clojure.java.io :as io]
             [aoc2023.util :as u]))
 
-(def joker-value 11)
-
-(defn compare-hand-values [h1 h2]
-  (cond
-    (and (empty? h1) (not-empty h2)) -1
-    (and (not-empty h1) (empty? h2)) 1
-    (and (empty? h1) (empty? h2)) 0
-    (and (not-empty h1) (not-empty h2))
-    (let [comparison (compare-cards (first h1) (first h2))]
-      (if (zero? comparison)
-        (recur (rest h1) (rest h2))
-        comparison))))
-
-(defn compare-hands [{type-value1 :type-value hand1 :hand} {type-value2 :type-value hand2 :hand}]
-  (let [comparison (compare type-value1 type-value2)]
-    (if (zero? comparison)
-      (compare-hand-values hand1 hand2)
-      comparison)))
-
-(defn get-value [c]
+(defn get-value [c jokerify?]
   (if (Character/isDigit c)
     (u/char->int c)
     (case c
       \A 14
       \K 13
       \Q 12
-      \J joker-value
+      \J (if jokerify? 0 11)
       \T 10)))
 
-(defn compare-cards [c1 c2]
-  (let [v1 (get-value c1)
-        v2 (get-value c2)]
+(defn compare-cards [c1 c2 jokerify?]
+  (let [v1 (get-value c1 jokerify?)
+        v2 (get-value c2 jokerify?)]
     (compare v1 v2)))
+
+(defn compare-hand-values [h1 h2 jokerify?]
+  (cond
+    (and (empty? h1) (not-empty h2)) -1
+    (and (not-empty h1) (empty? h2)) 1
+    (and (empty? h1) (empty? h2)) 0
+    (and (not-empty h1) (not-empty h2))
+    (let [comparison (compare-cards (first h1) (first h2) jokerify?)]
+      (if (zero? comparison)
+        (recur (rest h1) (rest h2) jokerify?)
+        comparison))))
+
+(defn compare-hands [jokerify? {type-value1 :type-value hand1 :hand} {type-value2 :type-value hand2 :hand}]
+  (let [comparison (compare type-value1 type-value2)]
+    (if (zero? comparison)
+      (compare-hand-values hand1 hand2 jokerify?)
+      comparison)))
 
 (defn count-cards [hand]
   (reduce (fn [counts card]
@@ -47,13 +45,13 @@
 (defn name-hand [counts]
   (let [sorted (vec (sort #(compare %2 %1) (vals counts)))]
     (match sorted
-      [5] [7 :five-of-kind]
-      [4 _] [6 :four-of-kind]
-      [3 2] [5 :full-house]
-      [3 _ _] [4 :three-of-kind]
-      [2 2 _] [3 :two-pair]
+      [5]       [7 :five-of-kind]
+      [4 _]     [6 :four-of-kind]
+      [3 2]     [5 :full-house]
+      [3 _ _]   [4 :three-of-kind]
+      [2 2 _]   [3 :two-pair]
       [2 _ _ _] [2 :pair]
-      :else [1 :high])))
+      :else     [1 :high])))
 
 (defn jokerify [counts]
   (let [jokers (get counts \J 0)
@@ -73,25 +71,19 @@
 (defn hand-value [hand]
   (* (:rank hand) (:bid hand)))
 
-(defn part1 [filename]
+(defn rank-cards [filename jokerify?]
   (->> filename u/load-input
-       (map (partial parse-row false))
-       (sort compare-hands)
+       (map (partial parse-row jokerify?))
+       (sort (partial compare-hands jokerify?))
        (map-indexed (fn [i hand] (assoc hand :rank (inc i))))
        (map hand-value)
        (apply +)))
 
-(defn part2 [filename]
-  (->> filename u/load-input
-       (map (partial parse-row true))
-       (sort compare-hands)
-       (map-indexed (fn [i hand] (assoc hand :rank (inc i))))
-       (map hand-value)
-       (apply +)))
+(defn part1 [filename] (rank-cards filename false))
+(defn part2 [filename] (rank-cards filename true))
 
 (u/varmista part1 "day7_example" 6440)
 (u/varmista part1 "day7_input" 253313241)
 
-(def joker-value 0) ; sorry....
 (u/varmista part2 "day7_example" 5905)
 (u/varmista part2 "day7_input" 253362743)
